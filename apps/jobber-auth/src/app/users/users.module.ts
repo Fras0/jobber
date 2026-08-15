6bc8437a223739ca84658ad86d1869ff8d638e89
Fras0 <frasasker0@gmail.com>
2026-08-15 20:43:09 +0300

    fixing unit test fails

--- apps/jobber-auth/src/app/users/users.module.ts
+++ apps/jobber-auth/src/app/users/users.module.ts
@@ -5,6 +5,6 @@ import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
     imports: [PrismaModule],
-            providers: [UsersResolver, UsersService]
+    providers: [UsersResolver, UsersService]
 })
 export class UsersModule { }
